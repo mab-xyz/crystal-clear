ab6c50f10fd08e7d82353e12e2e99ad4bac4a217
Monica Jin <monicachenjin@gmail.com>
2025-05-06 16:47:51 +0200

    refactor: get_label

--- webapp/backend/services/analysis_service.py
+++ webapp/backend/services/analysis_service.py
@@ -4,12 +4,12 @@ from loguru import logger
 from scsc.supply_chain import SupplyChain
 
 from web3 import Web3
-import requests
 
 from typing import Tuple
 
 from core.config import settings
 from core.exceptions import ContractAnalysisError, ExternalServiceError
+from core.metadata import get_labels
 
 
 def analyze_contract_dependencies(
@@ -113,39 +113,3 @@ def calculate_block_range(days: int) -> Tuple[int, int]:
     except Exception as e:
         logger.error(f"Error getting block range: {e}")
         raise ExternalServiceError(f"Failed to get block range: {str(e)}") from e
-
-def get_labels(addresses: list, ALLIUM_API_KEY: str) -> Optional[dict]:
-    """
-    Get labels for multiple Ethereum addresses from Etherscan.
-
-    Args:
-        addresses: List of Ethereum addresses to lookup
-
-    Returns:
-        dict: Dictionary mapping addresses to their labels
-    """
-    try:
-        params = ""
-        for address in addresses:
-            # lowercase the address to ensure consistency
-            params += f"'{address.lower()}',"
-        params = params[:-1]  # Remove the last comma
-        parameters = {"param_477":f"{params}"}
-        print(parameters)
-
-        response = requests.post(
-            "https://api.allium.so/api/v1/explorer/queries/g23nJaD4vABOS6utYocZ/run",
-            json=parameters,
-            headers={"X-API-Key": ALLIUM_API_KEY},
-        )
-
-        print(response.json())
-        json = response.json()
-        if 'data' in json and len(json['data']) > 0:
-            labels = {item['address']: item['name'] for item in json['data']}
-            return labels
-        else:
-            return None
-    except Exception as e:
-        print(f"Error getting labels for addresses {addresses}: {e}")
-        return None
